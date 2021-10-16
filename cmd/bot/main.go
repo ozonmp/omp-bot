7e0d5aac37c78bcc6b064192ba0a58ec38f689d3
Ivan Strelkov <ivstrelkov@ozon.ru>
2021-10-16 13:01:15 +0300

    First iteration

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+const token = "TOKEN"
 
 func main() {
-	fmt.Println("hello")
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.UpdateConfig{
+		Timeout: 60,
+	}
+
+	updates, err := bot.GetUpdatesChan(u)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	for update := range updates {
+		if update.Message == nil { // ignore any non-Message Updates
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "You wrote: "+update.Message.Text)
+
+		bot.Send(msg)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/istrel/bot
 
 go 1.17
+
+require github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
+
+require github.com/technoweenie/multipartstreamer v1.0.1 // indirect
