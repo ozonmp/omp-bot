da0be21a581dd316245ffcf266e9737d9329f3ab
VYBel <vbelkin@gmail.com>
2021-10-17 23:24:29 +0300

    RC-1

--- internal/app/commands/work/internship/command_list.go
+++ internal/app/commands/work/internship/command_list.go
@@ -1,8 +1,6 @@
 package internship
 
 import (
-	"encoding/json"
-	"github.com/VYBel/omp-bot/internal/app/path"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"log"
 )
@@ -15,26 +13,26 @@ func (c *WorkInternshipCommander) List(inputMessage *tgbotapi.Message) {
 		outputMsgText += c.internshipService.ShortString(p)
 		outputMsgText += "\n"
 	}
-
 	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, outputMsgText)
 
-	serializedData, _ := json.Marshal(CallbackListData{
-		Offset: 21,
-	})
+	/*
+		serializedData, _ := json.Marshal(CallbackListData{
+			Offset: 21,
+		})
 
-	callbackPath := path.CallbackPath{
-		Domain:       "work",
-		Subdomain:    "internship",
-		CallbackName: "list",
-		CallbackData: string(serializedData),
-	}
+		callbackPath := path.CallbackPath{
+			Domain:       "work",
+			Subdomain:    "internship",
+			CallbackName: "list",
+			CallbackData: string(serializedData),
+		}
 
-	msg.ReplyMarkup = tgbotapi.NewInlineKeyboardMarkup(
-		tgbotapi.NewInlineKeyboardRow(
-			tgbotapi.NewInlineKeyboardButtonData("Next page", callbackPath.String()),
-		),
-	)
-
+		msg.ReplyMarkup = tgbotapi.NewInlineKeyboardMarkup(
+			tgbotapi.NewInlineKeyboardRow(
+				tgbotapi.NewInlineKeyboardButtonData("Next page", callbackPath.String()),
+			),
+		)
+	*/
 	_, err := c.bot.Send(msg)
 	if err != nil {
 		log.Printf("WorkInternshipCommander.List: error sending reply message to chat - %v", err)
