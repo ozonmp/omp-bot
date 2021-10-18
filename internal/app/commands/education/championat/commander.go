522ce6933c137ebcca8313510c11eb80cd643d75
pojono <pojono@gmail.com>
2021-10-18 19:49:33 +0300

    add championat commander

---
+++ internal/app/commands/education/championat/commander.go
@@ -0,0 +1,53 @@
+package championat
+
+import (
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/ozonmp/omp-bot/internal/app/path"
+	"github.com/ozonmp/omp-bot/internal/service/education/championat"
+)
+
+type ChampionatCommander struct {
+	bot               *tgbotapi.BotAPI
+	championatService *championat.Service
+}
+
+func NewChampionatCommander(
+	bot *tgbotapi.BotAPI,
+) *ChampionatCommander {
+	championatService := championat.NewService()
+
+	return &ChampionatCommander{
+		bot:               bot,
+		championatService: championatService,
+	}
+}
+
+func (c *ChampionatCommander) HandleCallback(callback *tgbotapi.CallbackQuery, callbackPath path.CallbackPath) {
+	switch callbackPath.CallbackName {
+	case "list":
+		c.CallbackList(callback, callbackPath)
+	default:
+		log.Printf("ChampionatCommander.HandleCallback: unknown callback name: %s", callbackPath.CallbackName)
+	}
+}
+
+func (c *ChampionatCommander) HandleCommand(msg *tgbotapi.Message, commandPath path.CommandPath) {
+	switch commandPath.CommandName {
+	case "help":
+		c.Help(msg)
+	case "list":
+		c.List(msg)
+	case "get":
+		c.Get(msg)
+	case "delete":
+		c.Get(msg)
+	case "new":
+		c.New(msg)
+	case "edit":
+		c.Edit(msg)
+	default:
+		c.Default(msg)
+	}
+}
