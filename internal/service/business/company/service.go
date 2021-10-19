9e09172b37aa604183d50c45d9d43777022bfc5b
birkoffe <birkoff.anarchist@gmail.com>
2021-10-19 22:51:15 +0700

    remove old code

--- internal/service/business/company/service.go
+++ internal/service/business/company/service.go
@@ -17,23 +17,3 @@ type DummyCompanyService struct{}
 func NewDummyCompanyService() *DummyCompanyService {
 	return &DummyCompanyService{}
 }
-
-//
-
-// func (s *DummyCompanyService) List() []Company {
-// 	return allEntities
-// }
-
-// func (s *DummyCompanyService) Get(idx int) (*Company, error) {
-// 	return &allEntities[idx], nil
-// }
-
-// func (s *DummyCompanyService) Delete(idx int) (bool, error) {
-// 	if idx < 0 || idx > len(allEntities) {
-// 		return false, fmt.Errorf("Invalid index %d, max elements of companies - %d.", idx, len(allEntities))
-// 	}
-
-// 	allEntities = append(allEntities[:idx], allEntities[idx+1:]...)
-
-// 	return true, nil
-// }
