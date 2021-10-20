68af99487273c10aca39541a692ef892e6db301e
Develop-007 <52546506+Develop-007@users.noreply.github.com>
2021-10-20 23:26:43 +0300

    Add initials seats

--- internal/model/cinema/models.go
+++ internal/model/cinema/models.go
@@ -3,11 +3,41 @@ package cinema
 import "fmt"
 
 type Seat struct {
+	ID     uint64
 	Row    uint64
 	Number uint64
 	Price  uint64
 }
 
 func (r *Seat) String() string {
-	return fmt.Sprintf(`Seat (Row: %d; Number: %d) price: %d`, r.Row, r.Number, r.Price)
+	return fmt.Sprintf(`Seat #%d (Row: %d; Number: %d) price: %d`, r.ID, r.Row, r.Number, r.Price)
+}
+
+var InitialSeats = []Seat{
+	{
+		ID:     1,
+		Row:    1,
+		Number: 1,
+		Price:  100,
+	}, {
+		ID:     1,
+		Row:    1,
+		Number: 2,
+		Price:  100,
+	}, {
+		ID:     2,
+		Row:    2,
+		Number: 1,
+		Price:  150,
+	}, {
+		ID:     3,
+		Row:    2,
+		Number: 2,
+		Price:  150,
+	}, {
+		ID:     4,
+		Row:    3,
+		Number: 3,
+		Price:  200,
+	},
 }
