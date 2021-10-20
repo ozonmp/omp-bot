13cd016d21f44781a845630e45ba32ac516d0a6c
sanlex <sum93@yandex.ru>
2021-10-20 10:59:29 +0300

    help command minor fixes

--- internal/app/commands/payment/provider/command_help.go
+++ internal/app/commands/payment/provider/command_help.go
@@ -8,9 +8,9 @@ import (
 func (c *PaymentProviderCommander) Help(inputMessage *tgbotapi.Message) {
 	msg := tgbotapi.NewMessage(inputMessage.Chat.ID,
 		"/help__payment__provider - shows this help\n"+
-			"/get__payment__provider <id>- get provider by id\n"+
 			"/list__payment__provider - get all providers,\n"+
-			"/new__payment__provider <jsonString> - create new payment provider\n"+
+			"/get__payment__provider <id>- get provider by id\n"+
+			"/new__payment__provider <jsonString> - create new provider\n"+
 			"/delete__payment__provider <id> - deletes provider by id\n"+
 			"/edit__payment__provider <jsonString>- edit provider attributes",
 	)
