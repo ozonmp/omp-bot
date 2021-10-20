c563605d7126989f3f8075c23c1f3557be6a7c15
Satbek Turganbayev <set.turg@mail.ru>
2021-10-20 20:57:50 +0300

    fix help command output message

--- internal/app/commands/security/verification/command_help.go
+++ internal/app/commands/security/verification/command_help.go
@@ -8,7 +8,7 @@ import (
 
 func (c *SecurityVerificationCommander) Help(inputMessage *tgbotapi.Message) {
 	msg := tgbotapi.NewMessage(inputMessage.Chat.ID,
-		"/help - help\n"+
+		"/help__security__verification - help\n"+
 			"/list__security__verification - list products\n"+
 			"/get__security__verification - get product\n"+
 			"/delete__security__verification - delete product\n"+
