194aa6172219d0ce63fab22b6037cd190ea85e3e
podlinovvv <podlinovvv96@gmail.com>
2021-10-23 00:09:04 +0300

    up

--- internal/app/commands/work/course/command_delete.go
+++ internal/app/commands/work/course/command_delete.go
@@ -1,15 +1,27 @@
 package course
 
 import (
+	"fmt"
 	"log"
 	"strconv"
+	"strings"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
 func (c *WorkCourseCommander) Delete(inputMessage *tgbotapi.Message) {
 	args := inputMessage.CommandArguments()
+	args=strings.TrimSpace(args)
 	index, err := strconv.Atoi(args)
+
+	if err != nil {
+		msg := tgbotapi.NewMessage(inputMessage.Chat.ID, fmt.Sprintf("Error: wrong ID\n"))
+		_, err := c.bot.Send(msg)
+		if err != nil {
+			log.Printf("WorkCourseCommander.Delete: error sending error - %v", err)
+		}
+		return
+	}
 
 	result, err := c.courseService.Remove(uint64(index))
 
